77120bf21889a483f0d34b0734dbafcbd2f12727
tdibah <tdibah@example.com>
2026-05-13 14:11:42 +0100

    finish myfamily

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s "https://acad.learn2earn.ng/assets/superhero/all.json" | jq -rf family.jq | tr -d '"'
+curl -s "https://acad.learn2earn.ng/assets/superhero/all.json" | jq -rf family.jq | tr -d '"' | python3 -c "import sys; print(sys.stdin.read().replace(chr(92)+chr(110), chr(10)), end='')"
